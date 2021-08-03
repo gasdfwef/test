33b2d00c9f4774ea40a7aadf2f594c28394b313b
sonhosu <ghtn9398@nate.com>
2021-08-03 22:22:39 +0900

    Aad 초기환경세팅

---
+++ src/main/java/com/bteam/BteamProjectApplication.java
@@ -0,0 +1,13 @@
+package com.bteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BteamProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BteamProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bteam/BteamProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bteam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BteamProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
